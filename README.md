f413edd4b949e35d6e4b344d3ec10ae11e91b4d7
Rustom Shareef <rustom.shareef@gmail.com>
2023-01-25 22:36:34 -0500

    Repros 411 test failure

---
+++ README.md
@@ -0,0 +1,35 @@
+To repros a issue with Google Cloud Storage and `ObjectStore::rename_if_not_exists`
+
+Repro can be run with
+```
+cargo test
+```
+
+Fails with the following conditions as set by you in the Cargo.toml
+```
+# Fail defult-tls and native-tls both fail the test
+reqwest = { version = "0.11.14", default-features = false, features = ["default-tls"] }
+reqwest = { version = "0.11.14", default-features = false, features = ["native-tls"] }
+```
+
+Passes with the following conditions
+With `default-tls` and the patched `object_store`
+```
+reqwest = { version = "0.11.14", default-features = false, features = ["default-tls"] }
+
+[patch.crates-io]
+#object_store = { git = "https://github.com/glaredb/arrow-rs.git", branch = "content-length" }
+```
+
+With `native-tls` and the patched `object_store`
+```
+reqwest = { version = "0.11.14", default-features = false, features = ["native-tls"] }
+
+[patch.crates-io]
+#object_store = { git = "https://github.com/glaredb/arrow-rs.git", branch = "content-length" }
+```
+And with `rustls-tls` (this is what is used in the `object_store` crate
+```
+reqwest = { version = "0.11.14", default-features = false, features = ["rustls-tls"] }
+```
+
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,11 @@ fn main() {}
 #[cfg(test)]
 mod test {
     use std::env;
-    use std::sync::Arc;
 
     use bytes::Bytes;
     use object_store::gcp::GoogleCloudStorageBuilder;
     use object_store::path::Path;
     use object_store::ObjectStore;
-    use uuid::Uuid;
 
     #[tokio::test]
     async fn content_length_test() {
@@ -23,31 +21,13 @@ mod test {
             .build()
             .unwrap();
 
-        let store: Arc<dyn ObjectStore> = Arc::new(store);
+        let from = Path::from("a");
+        let to = Path::from("b");
 
-        let uuid = Uuid::new_v4();
-        let uuid2 = Uuid::new_v4();
+        let bytes = Bytes::from("test-data");
 
-        let other = tmp_path("a", &uuid, &uuid2);
-        let from = tmp_path("b", &uuid, &uuid2);
-        let to = visible_path("c", &uuid);
-
-        let bytes = Bytes::from("test-data-test-data");
-
-        store.put(&other, bytes.clone()).await.unwrap();
         store.put(&from, bytes).await.unwrap();
 
         store.rename_if_not_exists(&from, &to).await.unwrap();
-    }
-
-    fn visible_path(location: &str, db_id: &Uuid) -> Path {
-        Path::from(format!("databases/{}/visible/{}", db_id, location,))
-    }
-
-    fn tmp_path(location: &str, db_id: &Uuid, process_id: &Uuid) -> Path {
-        Path::from(format!(
-            "databases/{}/tmp/{}/{}",
-            db_id, process_id, location,
-        ))
     }
 }
